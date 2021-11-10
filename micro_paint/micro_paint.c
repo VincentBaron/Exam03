01e0106442d6b818b98ad02efa8de5a422f00c8f
Vincent BARON <vbaron@e2r9p6.clusters.42paris.fr>
2021-11-10 12:27:04 +0100

    changed drawing

--- micro_paint/micro_paint.c
+++ micro_paint/micro_paint.c
@@ -6,7 +6,7 @@
 /*   By: vbaron <vbaron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 10:43:15 by vbaron            #+#    #+#             */
-/*   Updated: 2021/11/09 16:09:42 by vbaron           ###   ########.fr       */
+/*   Updated: 2021/11/10 12:15:06 by vbaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,10 @@ int read_file(t_gen *micro)
         int ret;
         
         ret = fscanf(micro->fd, "%d %d %c\n", &micro->width, &micro->height, &micro->bgrd);
+        if (ret != 3)
+            return (0);
+        if (!(micro->width <= 300 && micro->width > 0) || !(micro->height <= 300 && micro->height > 0))
+            return (0);
         return (1);
 }
 
@@ -69,37 +73,39 @@ int display(t_gen *micro)
     return(1);
 }
 
+int is_in_rect(float i, float j, t_gen *micro)
+{
+    if (i < micro->start_hor || i > micro->start_hor + micro->sq_height || j < micro->start_hor || j > micro->start_hor + micro->sq_width)
+        return (0);
+    return (1);
+}
+
+int is_empty_rect(float i, float j, t_gen *micro)
+{
+     if (i < micro->start_vert || i > micro->start_vert + micro->sq_height || j < micro->start_hor || j > micro->start_hor + micro->sq_width)
+        return (0);
+    if (i - micro->start_vert < 1.0000000 || j - micro->start_hor < 1.00000000 || ((micro->start_vert + micro->sq_height) - i < 1.00000000) || ((micro->start_hor + micro->sq_width) - j < 1.00000000))
+        return (1);
+    return (0);
+}
+
 int draw_square(t_gen *micro)
 {
     int i;
     int j;
+    int R;
+    int r;
 
-    i = micro->start_vert + 0.5;
-    while (i < micro->height && i - (micro->start_vert + 0.5) < micro->sq_height)
+    i = 0;
+    while (i < micro->sq_height)
     {
-        j = micro->start_hor + 0.5;
-        while (j < micro->width && j - (micro->start_hor + 0.5) < micro->sq_width)
+        j = 0;
+        while (j < micro->sq_width)
         {
-            micro->array[(int)(j + micro->width * i)] = micro->color;
-            j++;
-        }
-        i++;
-    }
-    return (1);
-}
-
-int draw_empty_square(t_gen *micro)
-{
-    int i;
-    int j;
-
-    i = micro->start_vert + 0.5;
-    while (i < micro->height && i - (micro->start_vert + 0.5) < micro->sq_height)
-    {
-        j = micro->start_hor + 0.5;
-        while (j < micro->width && j - (micro->start_hor + 0.5) < micro->sq_width)
-        {
-            if (i == micro->start_vert + 0.5 || i == micro->start_vert + 0.5 + micro->sq_height - 1 || j == micro->start_hor + 0.5 || j == micro->start_hor + 0.5 + micro->sq_width - 1)
+            
+            r = is_empty_rect(i, j, micro);
+            R = is_in_rect(i, j, micro);
+            if ((r && micro->type == 'r') || (R && micro->type == 'R'))
                 micro->array[(int)(j + micro->width * i)] = micro->color;
             j++;
         }
@@ -110,15 +116,11 @@ int draw_empty_square(t_gen *micro)
 
 int draw_squares(t_gen *micro)
 {
-    int i;
     int ret;
 
     while ((ret = fscanf(micro->fd, "%c %f %f %f %f %c\n", &micro->type, &micro->start_hor, &micro->start_vert, &micro->sq_width, &micro->sq_height, &micro->color)) == 6)
     {
-        if (micro->type == 'R')
-            draw_square(micro);
-        else if (micro->type == 'r')
-            draw_empty_square(micro);
+        draw_square(micro);
     }
     return (1);
 }
